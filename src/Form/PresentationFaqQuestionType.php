34ba2643ebd9670856b8561fd355df982d0a9abf
razdoris <beurel.nicolas@gmail.com>
2021-09-29 15:36:25 +0200

    création d'un page d'administration

--- src/Form/PresentationFaqQuestionType.php
+++ src/Form/PresentationFaqQuestionType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\PresentationFaqQuestion;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,15 @@ class PresentationFaqQuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('number')
-            ->add('question')
-            ->add('answer')
+            ->add('number', NumberType::class,[
+                'label'=>"numero d'ordre"
+            ])
+            ->add('question', null,[
+                'label'=>"question"
+            ])
+            ->add('answer', CKEditorType::class,[
+                'label'=>'reponse a la question'
+            ])
         ;
     }
 
